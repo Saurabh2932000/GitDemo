27ba991013c746eae0e98bcfd431a9073e09bb82
Saurabh Jadhav <sj2932000@gmail.com>
2025-03-06 18:24:26 +0530

    Incomplete program

---
+++ MoveZeroToEnd.java
@@ -0,0 +1,11 @@
+public class MoveZeroToEnd {
+
+    public static void funMoveZeroToEnd(int arr[]){
+    
+        
+    }
+    public static void main(String args[]){
+        int arr[]={1,2,0,3,5,0,7,0};
+        funMoveZeroToEnd(arr);
+    }
+}
